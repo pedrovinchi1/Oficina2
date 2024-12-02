c010181561b79897e3b11d7762a380e848d37400
Pedro Vinchi <133929538+pedrovinchi1@users.noreply.github.com>
2024-12-02 18:54:14 -0300

    pedrovinchi1/Oficinas2/#1

--- app/test_main.py
+++ app/test_main.py
@@ -5,15 +5,14 @@ from sqlalchemy.orm import sessionmaker
 from app.main import app
 from app.database import Base, get_db
 
-# Configuração do banco de dados de teste
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/certificados"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Criação do banco de dados de teste
 Base.metadata.create_all(bind=engine)
 
-# Dependência de sessão de banco de dados de teste
 def override_get_db():
     try:
         db = TestingSessionLocal()
@@ -40,14 +39,12 @@ def test_login(test_client):
     assert "access_token" in response.cookies
 
 def test_create_oficina(test_client):
-    # Primeiro, faça o login para obter o token de acesso
     response = test_client.post("/login", data={"username": "test@example.com", "password": "testpassword"})
     assert response.status_code == 200
     token = response.cookies.get("access_token")
 
-    # Em seguida, crie uma oficina usando o token de acesso
     response = test_client.post("/create-oficina", data={"titulo": "Test Oficina", "descricao": "Descrição da Test Oficina"}, cookies={"access_token": token})
-    assert response.status_code == 302  # Redirecionamento para o dashboard
+    assert response.status_code == 302  
 
 def test_read_dashboard(test_client):
     response = test_client.get("/dashboard")
